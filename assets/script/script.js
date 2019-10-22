5c1c5eef35aa3fdc7b10126a3f82e5d1d3cafafc
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2019-10-22 14:08:33 +0100

    main structure added

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,3 +3,23 @@ console.log(navItem);
 navItem.forEach(function(element, index) {
   console.log(element.innerText)
 })
+
+const para = document.querySelector('.paragraph');
+console.log(para)
+para.addEventListener('mouseover', function() {
+  this.classList.add('active');
+})
+
+para.addEventListener('mouseleave', function() {
+  this.classList.remove('active');
+})
+
+const testLink = document.querySelector('.test');
+console.log(testLink);
+testLink.addEventListener('mouseover', function() {
+  this.classList.add('active');
+})
+
+testLink.addEventListener('mouseleave', function() {
+  this.classList.remove('active');
+})
