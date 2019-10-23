aa0aa94b1ad27b6ed8dd3956ac861ee2758cd38f
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2019-10-23 10:13:30 +0100

    'about' section added

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,25 +1,14 @@
-const navItem = document.querySelectorAll('.nav__item');
-console.log(navItem);
-navItem.forEach(function(element, index) {
-  console.log(element.innerText)
-})
+const navList = document.querySelectorAll('.nav__item');
 
-const para = document.querySelector('.paragraph');
-console.log(para)
-para.addEventListener('mouseover', function() {
-  this.classList.add('active');
-})
+console.log(navList);
+for(let i=0; i<navList.length; i++) {
+  navList[i].addEventListener('mouseover', function() {
+    this.classList.add('active');
+  })
+}
 
-para.addEventListener('mouseleave', function() {
-  this.classList.remove('active');
-})
-
-const testLink = document.querySelector('.test');
-console.log(testLink);
-testLink.addEventListener('mouseover', function() {
-  this.classList.add('active');
-})
-
-testLink.addEventListener('mouseleave', function() {
-  this.classList.remove('active');
-})
+for(let i=0; i<navList.length; i++) {
+  navList[i].addEventListener('mouseleave', function() {
+    this.classList.remove('active');
+  })
+}
