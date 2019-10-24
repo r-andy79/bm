18d3dc164aab16eef6c8ddcac16aec43850dd7e2
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2019-10-24 10:56:31 +0100

    menu bottom border added on hover in js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,14 +1,28 @@
 const navList = document.querySelectorAll('.nav__item');
 
 console.log(navList);
-for(let i=0; i<navList.length; i++) {
-  navList[i].addEventListener('mouseover', function() {
-    this.classList.add('active');
-  })
-}
+// for(let i=0; i<navList.length; i++) {
+//   navList[i].addEventListener('mouseover', function() {
+//     this.classList.add('active');
+//   })
+// }
 
-for(let i=0; i<navList.length; i++) {
-  navList[i].addEventListener('mouseleave', function() {
-    this.classList.remove('active');
+// for(let i=0; i<navList.length; i++) {
+//   navList[i].addEventListener('mouseleave', function() {
+//     this.classList.remove('active');
+//   })
+// }
+
+navList.forEach(function(element) {
+  element.addEventListener('mouseover', function() {
+    element.classList.add('active');
+  });
+})
+
+navList.forEach(function(element) {
+  element.addEventListener('mouseleave', function() {
+    element.classList.remove('active');
   })
-}
+})
+
+
